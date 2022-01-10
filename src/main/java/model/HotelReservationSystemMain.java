6a085780a958e11db6e04056ac72b91adc218066
Achyuth Naidu <ashwath.bly@gamil.com>
2022-01-10 19:29:48 +0530

    UC6 :- Ability to find the cheapest best rated hotel Hotel for a given Date Range

--- src/main/java/model/HotelReservationSystemMain.java
+++ src/main/java/model/HotelReservationSystemMain.java
@@ -13,12 +13,14 @@ import serivce.Hotel;
 
 
 public class HotelReservationSystemMain {
+	
 	private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd-MMM-yyyy");
 	static ArrayList<Hotel> hotel_list = new ArrayList<>();
 	static Scanner sc = new Scanner(System.in);
 
 	/*
-	 * UC1 :-Ability to add Hotel in a Hotel Reservation System with Name and rates for Regular Customer...
+	 * UC1 :-Ability to add Hotel in a Hotel Reservation System with Name and rates
+	 * for Regular Customer...
 	 * 
 	 * UC3 :- Ability to add weekday and weekend rates for each Hotel
 	 * 
@@ -36,7 +38,10 @@ public class HotelReservationSystemMain {
 	}
 
 	/*
-	 * UC2, UC4
+	 * UC2 :- Ability to find the cheapest Hotel for a given Date Range
+	 * 
+	 * UC4 :- Ability to find the cheapest Hotel for a given Date Range based on
+	 * weekday and weekend - I/P – 11Sep2020, 12Sep2020
 	 */
 	public static boolean findTheCheaptestHotelAndTotalCost(String stateDate, String endDate) {
 		LocalDate stateDateLD = LocalDate.parse(stateDate, DATE_FORMAT);
@@ -57,5 +62,28 @@ public class HotelReservationSystemMain {
 		return true;
 	}
 
+	/**
+	 * UC6 :- Ability to find the cheapest best rated hotel Hotel for a given Date Range
+	 * 
+	 */
+	public static boolean findTheCheaptestBestRatingHotel(String stateDate, String endDate) {
 
+		final LocalDate stateDateLD = LocalDate.parse(stateDate, DATE_FORMAT);
+		final LocalDate endDateLD = LocalDate.parse(endDate, DATE_FORMAT);
+		int daysBetween = (int) ChronoUnit.DAYS.between(stateDateLD, endDateLD);
+		List<Object> storedCheapRateHotel = hotel_list.stream().map(hotelData -> {
+			Hotel result = new Hotel();
+			result.setName(hotelData.getName());
+			result.setTotalRate(hotelData.getWeekendRate() + hotelData.getWeekdayRate() * daysBetween);
+			result.setTypeOfCustomer(hotelData.getTypeOfCustomer());
+			result.setWeekdayRate(hotelData.getWeekdayRate());
+			result.setWeekendRate(hotelData.getWeekendRate());
+			result.setRating(hotelData.getRating());
+			return result;
+		}).sorted(Comparator.comparing(Hotel::getTotalRate).thenComparing(Hotel::getRating))
+				.collect(Collectors.toList());
+		System.out.printf("The total number of the days is : %d\n", daysBetween);
+		storedCheapRateHotel.forEach(System.out::println);
+		return true;
+	}
 }
--- src/test/java/hotelReservatonTest/HotelReservationTest.java
+++ src/test/java/hotelReservatonTest/HotelReservationTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 
 import model.HotelReservationSystemMain;
 
-
 public class HotelReservationTest {
 
 	@Test
@@ -16,5 +15,16 @@ public class HotelReservationTest {
 //				Hotel [name=Bridgewood, totalRate=0, weekendRate=150, weekdayRate=50, rating=4]
 //				Hotel [name=Lakewood, totalRate=0, weekendRate=110, weekdayRate=90, rating=3]
 //				Hotel [name=Ridgewood, totalRate=0, weekendRate=220, weekdayRate=150, rating=5]
+	}
+
+	@Test
+	public void findTheCheaptestHotelAndTotalCostTest() {
+		HotelReservationSystemMain.addHotels();
+		boolean cheapHotel = HotelReservationSystemMain.findTheCheaptestHotelAndTotalCost("11-Sep-2020", "12-Sep-2020");
+		Assert.assertEquals(true, cheapHotel);
+//		Output:-
+//				Hotel [name=Bridgewood, totalRate=200, weekendRate=150, weekdayRate=50, rating=4]
+//				Hotel [name=Lakewood, totalRate=200, weekendRate=110, weekdayRate=90, rating=3]
+
 	}
 }
